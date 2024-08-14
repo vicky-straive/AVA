84b186c95e6ce4c638c0fdccfd433b4abea15db3
vignesh.muralimohan <vignesh.muralimohan@straive.com>
2024-08-14 15:28:28 +0530

    fix v1.7

--- next.config.js
+++ next.config.js
@@ -1,25 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(js|jsx|ts|tsx)$/, // Match JS, JSX, TS, and TSX files
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['next/babel'], // Use Next.js' Babel preset without needing a separate file
-        },
-      },
-      exclude: /node_modules/,
-    });
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        and: [/\.(js|ts)x?$/],
-      },
-      use: [{ loader: '@svgr/webpack' }],
-    });
-    return config;
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
