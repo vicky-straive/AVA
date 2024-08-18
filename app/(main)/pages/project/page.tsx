0d2b1a08a5c06c5ceddc2cef474bfd58a228c68b
vicky-straive <vignesh.muralimohan@straive.com>
2024-08-18 09:16:52 +0530

    fix v1.15

--- app/(main)/pages/project/page.tsx
+++ app/(main)/pages/project/page.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ListDemo from '../../uikit/project_list/page';
 
 const ProjectPage = () => {
     return (
-        <div className="grid">
-            <div className="col-12">
-                <div className="card">
-                    <h5>Projects</h5>
-                    <p>List of all project details will be listed here</p>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div className="grid">
+                <div className="col-12">
+                    <div className="card">
+                        <h5>Projects</h5>
+                        <p>List of all project details will be listed here</p>
+                    </div>
+                    <ListDemo />
                 </div>
-                <ListDemo />
             </div>
-        </div>
+        </Suspense>
     );
 };
 
